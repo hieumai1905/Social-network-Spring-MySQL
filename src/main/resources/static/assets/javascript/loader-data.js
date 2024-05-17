56c1f8531455d218d9dbfa0f1f4dba44289031c2
hieu mai <maivanhieu19052002@gmail.com>
2024-05-18 05:36:31 +0700

    Fix split conversation group and personal

--- src/main/resources/static/assets/javascript/loader-data.js
+++ src/main/resources/static/assets/javascript/loader-data.js
@@ -2,15 +2,11 @@ let conversations = [];
 
 function loadConversations() {
     $.ajax({
-        url: '/api/conversations',
-        type: 'GET',
-        success: function (data) {
+        url: '/api/conversations', type: 'GET', success: function (data) {
             conversations = data;
             console.log('Conversations loaded');
-            console.log(conversations);
             displayConversations();
-        },
-        error: function (data) {
+        }, error: function (data) {
             console.log('Error loading conversations');
         }
     });
