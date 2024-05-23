e522ae481f1938d5303bd4bae11f8c15b7c24503
Hieu Mai <maivanhieu19052002@gmail.com>
2024-05-24 01:08:20 +0700

    Add show list post by post interact

--- src/main/java/com/socialnetwork/socialnetworkjavaspring/services/new_feeds/NewsFeedService.java
+++ src/main/java/com/socialnetwork/socialnetworkjavaspring/services/new_feeds/NewsFeedService.java
@@ -1,18 +1,16 @@
 package com.socialnetwork.socialnetworkjavaspring.services.new_feeds;
 
 import com.socialnetwork.socialnetworkjavaspring.models.Post;
-import com.socialnetwork.socialnetworkjavaspring.models.Comment;
-import com.socialnetwork.socialnetworkjavaspring.models.CommentReply;
 import com.socialnetwork.socialnetworkjavaspring.services.likes.ILikeService;
 import com.socialnetwork.socialnetworkjavaspring.services.posts.IPostService;
+import com.socialnetwork.socialnetworkjavaspring.services.posts.PostGeneralService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Comparator;
 import java.util.List;
 
 @Service
-public class NewsFeedService implements INewsFeedService {
+public class NewsFeedService extends PostGeneralService implements INewsFeedService {
     @Autowired
     private IPostService postService;
 
@@ -30,22 +28,5 @@ public class NewsFeedService implements INewsFeedService {
             sortCommentsAndReplies(post);
         });
         return posts;
-    }
-
-    private void setLikedStatusForCommentsAndReplies(Post post, String userId) {
-        post.getComments().forEach(comment -> {
-            boolean isLikedComment = likeService.existsByCommentIdAndUserId(comment.getCommentId(), userId);
-            comment.setLiked(isLikedComment);
-            comment.getCommentReplies().forEach(reply -> {
-                boolean isLikedReply = likeService.existsByCommentReplyIdAndUserId(reply.getCommentReplyId(), userId);
-                reply.setLiked(isLikedReply);
-            });
-        });
-    }
-
-    private void sortCommentsAndReplies(Post post) {
-        post.getComments().sort(Comparator.comparing(Comment::getCommentAt));
-        post.getComments().forEach(comment -> comment.getCommentReplies()
-                .sort(Comparator.comparing(CommentReply::getReplyAt)));
     }
 }
