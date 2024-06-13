12269a6dceab078ed304a7bf14daac059e782c4b
Hieu Mai <maivanhieu19052002@gmail.com>
2024-06-13 19:02:58 +0700

    Add change new email confirm from new email

--- src/main/java/com/socialnetwork/socialnetworkjavaspring/controllers/SettingsController.java
+++ src/main/java/com/socialnetwork/socialnetworkjavaspring/controllers/SettingsController.java
@@ -2,6 +2,7 @@ package com.socialnetwork.socialnetworkjavaspring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
