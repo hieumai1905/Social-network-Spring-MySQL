c35ae1db33e679e53d3cc9c58f298bc8541f74a8
Hieu Mai <maivanhieu19052002@gmail.com>
2024-06-13 22:39:06 +0700

    Add update information account

--- src/main/java/com/socialnetwork/socialnetworkjavaspring/controllers/SettingsController.java
+++ src/main/java/com/socialnetwork/socialnetworkjavaspring/controllers/SettingsController.java
@@ -1,11 +1,17 @@
 package com.socialnetwork.socialnetworkjavaspring.controllers;
 
+import com.socialnetwork.socialnetworkjavaspring.DTOs.users.UserInfoUpdateDTO;
 import com.socialnetwork.socialnetworkjavaspring.DTOs.users.UserPasswordUpdateDTO;
+import com.socialnetwork.socialnetworkjavaspring.models.enums.Gender;
 import com.socialnetwork.socialnetworkjavaspring.services.users.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static com.socialnetwork.socialnetworkjavaspring.utils.Constants.*;
 
@@ -51,6 +57,26 @@ public class SettingsController extends ApplicationController {
             userService.save(currentUser);
             modelAndView.addObject("error", "Update password successfully!");
         }
+        return setAuthor(modelAndView);
+    }
+
+    @PostMapping("/update-info")
+    public ModelAndView updateInfo(@ModelAttribute UserInfoUpdateDTO userInfoUpdateDTO) {
+        ModelAndView modelAndView = new ModelAndView("settings/index");
+        modelAndView.addObject("account", true);
+        currentUser.setFullName(userInfoUpdateDTO.getFullName());
+        Gender gender = Gender.valueOf(userInfoUpdateDTO.getGender());
+        currentUser.setGender(gender);
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            Date birthDay = dateFormat.parse(userInfoUpdateDTO.getBirthday());
+            currentUser.setDob(birthDay);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        currentUser.setCountry(userInfoUpdateDTO.getCountry());
+        currentUser.setAboutMe(userInfoUpdateDTO.getDescription());
+        userService.save(currentUser);
         return setAuthor(modelAndView);
     }
 }
